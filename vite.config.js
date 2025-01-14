ce8c7224febcaad62a9ad007592cccec754da078
Obed-Olavarria <ing.olavarria.mannager@gmail.com>
2025-01-13 22:15:16 -0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: 'https://Obed-Olavarria.github.io/Curriculum_Digital_Obed_Olavarria'
 })
