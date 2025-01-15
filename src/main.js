6ab2a7ebb3bd27a8131305292458325bd7476733
Obed-Olavarria <ing.olavarria.mannager@gmail.com>
2025-01-15 13:10:49 -0600

    Estrcutura

--- src/main.js
+++ src/main.js
@@ -1,5 +1,7 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import './style.css';
+import App from './App.vue';
+import router from './router'; // Importa Vue Router
 
-createApp(App).mount('#app')
+// Crea una sola instancia de la app y usa Vue Router
+createApp(App).use(router).mount('#app');
--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/CV_Obed_Olavarria/' // Ajusta el nombre del repositorio si es necesario
+  //base: '/CV_Obed_Olavarria/'  Ajusta el nombre del repositorio si es necesario
 });
