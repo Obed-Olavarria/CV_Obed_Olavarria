fbdedd5945f48d60e582da330e032e04c9d54973
Obed-Olavarria <ing.olavarria.mannager@gmail.com>
2025-01-14 23:57:34 -0600

    1

--- vite.config.js
+++ vite.config.js
@@ -4,13 +4,5 @@ import vue from '@vitejs/plugin-vue';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/Curriculum_Digital_Obed_Olavarria/', // Ajusta el nombre del repositorio si es necesario
-  build: {
-    outDir: 'dist', // Directorio de salida de la compilación
-  },
-  resolve: {
-    alias: {
-      '@': '/src',  // Esto asegura que la ruta "@/assets" sea resuelta correctamente
-    },
-  },
+  base: '/CV_Obed_Olavarria/' // Ajusta el nombre del repositorio si es necesario
 });
